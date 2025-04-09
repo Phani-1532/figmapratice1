7d393393e389940bc1c65dd07af88c88a88a4a39
Phanindra <phanindragadireddy2@gmail.com>
2025-04-09 14:51:20 +0530

    ui created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Header from './components/header/Header'
 import Section from './components/section/Section'
 import Grid from './components/grids/Grid'
+import Api from './components/Api/Api'
 
 const App = () => {
   return (
     <>
       <Header />
       <Section />
-      <Grid />
+      <Grid  />
+      <Api />
+      
     </>
   )
 }
